e3378cc385986b0b42561ba999f99422967de762
MaRtin322s <marti.sofroniev0408@gmail.com>
2022-12-02 14:55:31 +0200

    add data-price attribute

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,9 @@
 import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
-  // This block will be executed once the page is loaded and ready
+    // This block will be executed once the page is loaded and ready
 
-
+    const products = Array.from(document.querySelectorAll('.product'));
+    const price = document.querySelector('.price');
+    products.forEach(x => x.setAttribute('data-price', price.textContent));
 });
